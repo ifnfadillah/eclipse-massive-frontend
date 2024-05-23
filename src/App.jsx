a5473b0f9738e57ffc22fd9ca0642f8e78d7f4ca
ifnfadillah <143673321+ifnfadillah@users.noreply.github.com>
2024-05-23 13:19:29 +0700

    add 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { Route, Routes } from "react-router-dom";
+import Layout from "./layout/layout";
 import HomeDashboard from "./admin/dashboard/HomeDashboard";
 import Login from "./admin/login/Login";
 import HomeUser from "./user/home/HomeUser";
 import Kidspedia from "./user/home/edukasi/Kidspedia";
 import Sharenting from "./user/home/Sharenting";
 import PanduanAsuh from "./user/home/PanduanAsuh";
+import NotFound from "./pages/404";
+
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomeUser />} />
-        <Route path="/kidspedia" element={<Kidspedia />} />
-        <Route path="/sharenting" element={<Sharenting />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<HomeDashboard />} />
-        <Route path="/edukasi/panduanasuh" element={<PanduanAsuh />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/beranda" element={<HomeUser />} />
+      <Route path="/kidspedia" element={<Kidspedia />} />
+      <Route path="/sharenting" element={<Sharenting />} />
+      <Route path="/edukasi/panduanasuh" element={<PanduanAsuh />} />
+      <Route path="*" element={<NotFound />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Layout />}>
+        <Route index path="/" element={<HomeDashboard />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
