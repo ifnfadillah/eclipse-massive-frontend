15704187275b568dfcb262df207be9b8602cd7d7
fptra28 <faturrahman86.fr@gmail.com>
2024-05-26 14:04:30 +0700

    Add some in Article

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,18 @@ import HomeUser from "./user/home/HomeUser";
 import Kidspedia from "./user/home/edukasi/Kidspedia";
 import Sharenting from "./user/home/Sharenting";
 import PanduanAsuh from "./user/home/PanduanAsuh";
+import ListArticle from "./user/home/listArtikel";
 import NotFound from "./pages/404";
-import './index.css';
-
+import "./index.css";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomeUser />}>
-        <Route index path="/" element={<HomeUser />} />
-        <Route path="/kidspedia" element={<Kidspedia />} />
-        <Route path="/sharenting" element={<Sharenting />} />
-        <Route path="/edukasi/panduanasuh" element={<PanduanAsuh />} />
-      </Route>
+      <Route index path="/" element={<HomeUser />} />
+      <Route path="/kidspedia" element={<Kidspedia />} />
+      <Route path="/sharenting" element={<Sharenting />} />
+      <Route path="/edukasi/panduanasuh" element={<PanduanAsuh />} />
+      <Route path="/article" element={<ListArticle />} />
       <Route path="*" element={<NotFound />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Layout />}>
---
+++ src/user/home/listArtikel.jsx
@@ -0,0 +1,15 @@
+import NavbarHome from "../components/NavbarHome";
+import Footer from "../components/Footer";
+import ListAarticle from "../section/SectionCTAarticle";
+
+function listArticle() {
+  return (
+    <>
+      <NavbarHome />
+      <ListAarticle />
+      <Footer />
+    </>
+  );
+}
+
+export default listArticle;
